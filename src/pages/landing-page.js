298601c9971601cb091292dfcf53bb0868d25173
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-04-27 23:24:21 +0100

    feat(frontend): populated the list from the form here

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -1,5 +1,6 @@
 import { Box, Grid, makeStyles } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
+import AddNewStory from '../components/dialogs/add-new-story'
 
 const useStyles = makeStyles(theme => ({
   customer: {
@@ -222,169 +223,208 @@ const customerStories2 = [
 
 const LandingPage = () => {
   const classes = useStyles()
+  const [openModal, setOpenModal] = useState(false)
+  const [storyList, setStoryList] = useState([])
+
+  const handleClose = () => setOpenModal(false)
 
   return (
-    <Box>
-      <Box className={classes.wonderfullCustomer}>
-        <Box>
-          <Box component='h1' className={classes.heading}>
-            Amazing <br /> Experiences from Our <br /> Wonderful Customers
+    <>
+      <Box>
+        <Box className={classes.wonderfullCustomer}>
+          <Box>
+            <Box component='h1' className={classes.heading}>
+              Amazing <br /> Experiences from Our <br /> Wonderful Customers
+            </Box>
+            <Box component='p' fontSize='18px' fontWeight='400' color='#494949'>
+              Here is what customers and vendors are saying about <br /> us, you
+              can share your stories with us too.
+            </Box>
           </Box>
-          <Box component='p' fontSize='18px' fontWeight='400' color='#494949'>
-            Here is what customers and vendors are saying about <br /> us, you
-            can share your stories with us too.
-          </Box>
+          <Box component='img' src='/images/Group 81.svg' maxWidth='100%' />
         </Box>
-        <Box component='img' src='/images/Group 81.svg' maxWidth='100%' />
-      </Box>
 
-      <Box className={classes.customer}>
-        <Box component='img' src='/images/Group 89.svg' />
-        <Box className={classes.experienceContainer}>
-          <Box component='h2' className={classes.title}>
-            Tolu & Joy’s Experience
-          </Box>
-          <Box
-            border='1px solid rgba(255, 255, 255, 0.4)'
-            borderRadius='4px'
-            color='white'
-            px={4}
-            width='6rem'
-            display='flex'
-            justifyContent='center'
-            mb={8}
-          >
-            CUSTOMER
-          </Box>
-          <Box component='p' color='white' fontSize='18px'>
-            I had the best experience shopping with vasiti. Usability of the
-            website was great, very good customer service, an all round great
-            experience. I would definately be coming back! I had the best
-            experience shopping with vasiti. Usability of the website was great,
-            very good customer service, an all round great experience. I would
-            definately be coming back!
-          </Box>
-          <Box className={classes.shareStyle}>
+        <Box className={classes.customer}>
+          <Box component='img' src='/images/Group 89.svg' />
+          <Box className={classes.experienceContainer}>
+            <Box component='h2' className={classes.title}>
+              Tolu & Joy’s Experience
+            </Box>
             <Box
-              component='p'
+              onClick={() => setOpenModal(true)}
+              border='1px solid rgba(255, 255, 255, 0.4)'
+              borderRadius='4px'
               color='white'
-              m={0}
-              className={classes.shareStory}
+              px={4}
+              width='6rem'
+              display='flex'
+              justifyContent='center'
+              mb={8}
             >
-              Share your own story!
+              CUSTOMER
             </Box>
-            <Box component='img' src='/images/Vector 3.svg' maxWidth='100%' />
+            <Box component='p' color='white' fontSize='18px'>
+              I had the best experience shopping with vasiti. Usability of the
+              website was great, very good customer service, an all round great
+              experience. I would definately be coming back! I had the best
+              experience shopping with vasiti. Usability of the website was
+              great, very good customer service, an all round great experience.
+              I would definately be coming back!
+            </Box>
+            <Box className={classes.shareStyle}>
+              <Box
+                component='p'
+                color='white'
+                m={0}
+                className={classes.shareStory}
+              >
+                Share your own story!
+              </Box>
+              <Box component='img' src='/images/Vector 3.svg' maxWidth='100%' />
+            </Box>
           </Box>
         </Box>
-      </Box>
 
-      <Box>
-        <Box className={classes.gridContainer}>
-          <Grid container spacing={6}>
-            {customerStories.map(
-              ({
-                id,
-                customerImg,
-                customerName,
-                location,
-                vendor,
-                message
-              }) => (
-                <Grid key={id} item xs={12} md={4}>
-                  <Box component='img' src={customerImg} />
-                  <Box
-                    component='h3'
-                    color='#474747'
-                    fontSize='24px'
-                    fontWeight='600'
-                    mb={0}
-                  >
-                    {customerName}
-                  </Box>
-                  <Box display='flex' alignItems='center'>
+        <Box>
+          <Box className={classes.gridContainer}>
+            <Grid container spacing={6}>
+              {customerStories.map(
+                ({
+                  id,
+                  customerImg,
+                  customerName,
+                  location,
+                  vendor,
+                  message
+                }) => (
+                  <Grid key={id} item xs={12} md={4}>
+                    <Box component='img' src={customerImg} />
                     <Box
-                      component='p'
-                      color='#535353'
-                      fontSize='15px'
-                      fontWeight='500'
+                      component='h3'
+                      color='#474747'
+                      fontSize='24px'
+                      fontWeight='600'
+                      mb={0}
                     >
-                      {location}
+                      {customerName}
                     </Box>
-                    <Box
-                      component='span'
-                      className={`${classes.generalStyle} ${
-                        vendor === 'CUSTOMER'
-                          ? classes.customerStyle
-                          : classes.vendorStyle
-                      }`}
-                    >
-                      {vendor}
+                    <Box display='flex' alignItems='center'>
+                      <Box
+                        component='p'
+                        color='#535353'
+                        fontSize='15px'
+                        fontWeight='500'
+                      >
+                        {location}
+                      </Box>
+                      <Box
+                        component='span'
+                        className={`${classes.generalStyle} ${
+                          vendor === 'CUSTOMER'
+                            ? classes.customerStyle
+                            : classes.vendorStyle
+                        }`}
+                      >
+                        {vendor}
+                      </Box>
                     </Box>
-                  </Box>
-                  <Box component='p' fontSize='16px' fontWeight='400'>
-                    {message}
-                  </Box>
-                </Grid>
-              )
-            )}
-          </Grid>
-        </Box>
-      </Box>
-
-      <Box className={classes.customer2}>
-        <Box maxWidth='30%' mr={18} mt={8} className={classes.josiahExperience}>
-          <Box component='h2' className={classes.heading2}>
-            Josiah’s Experience
+                    <Box component='p' fontSize='16px' fontWeight='400'>
+                      {message}
+                    </Box>
+                  </Grid>
+                )
+              )}
+            </Grid>
           </Box>
+        </Box>
+
+        <Box className={classes.customer2}>
           <Box
-            border='1px solid rgba(255, 92, 0, 0.4)'
-            borderRadius='4px'
-            color='#FF5C00'
-            px={4}
-            width='5rem'
-            display='flex'
-            justifyContent='center'
-            mb={8}
+            maxWidth='30%'
+            mr={18}
+            mt={8}
+            className={classes.josiahExperience}
           >
-            VENDOR
-          </Box>
-          <Box component='p' color='#242120' fontSize='18px'>
-            I had the best experience shopping with vasiti. Usability of the
-            website was great, very good customer service, an all round great
-            experience. I would definately be coming back! I had the best
-            experience shopping with vasiti. Usability of the website was great,
-            very good customer service, an all round great experience. I would
-            definately be coming back!
-          </Box>
-          <Box mt={8}>
-            <Box
-              component='p'
-              color='#FF5C00'
-              m={0}
-              className={classes.shareStory}
-            >
-              Share your own story!
+            <Box component='h2' className={classes.heading2}>
+              Josiah’s Experience
             </Box>
-            <Box component='img' src='/images/Vector 3.svg' />
+            <Box
+              border='1px solid rgba(255, 92, 0, 0.4)'
+              borderRadius='4px'
+              color='#FF5C00'
+              px={4}
+              width='5rem'
+              display='flex'
+              justifyContent='center'
+              mb={8}
+            >
+              VENDOR
+            </Box>
+            <Box component='p' color='#242120' fontSize='18px'>
+              I had the best experience shopping with vasiti. Usability of the
+              website was great, very good customer service, an all round great
+              experience. I would definately be coming back! I had the best
+              experience shopping with vasiti. Usability of the website was
+              great, very good customer service, an all round great experience.
+              I would definately be coming back!
+            </Box>
+            <Box mt={8}>
+              <Box
+                component='p'
+                color='#FF5C00'
+                m={0}
+                className={classes.shareStory}
+              >
+                Share your own story!
+              </Box>
+              <Box component='img' src='/images/Vector 3.svg' />
+            </Box>
           </Box>
+          <Box component='img' src='/images/woman-shoppingbag-card 1.svg' />
         </Box>
-        <Box component='img' src='/images/woman-shoppingbag-card 1.svg' />
-      </Box>
 
-      <Box>
-        <Box className={classes.gridContainer}>
-          <Grid container spacing={6}>
-            {customerStories2.map(
-              ({
-                id,
-                customerImg,
-                customerName,
-                location,
-                vendor,
-                message
-              }) => (
-                <Grid key={id} item xs={12} md={4}>
-                  <Box component='img' src={customerImg} />
+        <Box>
+          <Box className={classes.gridContainer}>
+            <Grid container spacing={6}>
+              {customerStories2.map(
+                ({
+                  id,
+                  customerImg,
+                  customerName,
+                  location,
+                  vendor,
+                  message
+                }) => (
+                  <Grid key={id} item xs={12} md={4}>
+                    <Box component='img' src={customerImg} />
+                    <Box
+                      component='h3'
+                      fontSize='24px'
+                      fontWeight='600'
+                      color='#474747'
+                      mb={0}
+                    >
+                      {customerName}
+                    </Box>
+                    <Box display='flex' alignItems='center'>
+                      <Box component='p'>{location}</Box>
+                      <Box
+                        ml={0}
+                        component='span'
+                        className={`${classes.vendorStyle} ${classes.genStyle}`}
+                      >
+                        {vendor}
+                      </Box>
+                    </Box>
+                    <Box component='p' fontSize='16px' fontWeight='400'>
+                      {message}
+                    </Box>
+                  </Grid>
+                )
+              )}
+              {storyList.map((story, storyIdx) => (
+                <Grid key={storyIdx} item xs={12} md={4}>
+                  <Box component='img' src={story.profileImg} />
                   <Box
                     component='h3'
                     fontSize='24px'
@@ -392,28 +432,34 @@ const LandingPage = () => {
                     color='#474747'
                     mb={0}
                   >
-                    {customerName}
+                    {story.firstName} {story.lastName}
                   </Box>
                   <Box display='flex' alignItems='center'>
-                    <Box component='p'>{location}</Box>
+                    {/* <Box component='p'>{location}</Box> */}
                     <Box
                       ml={0}
                       component='span'
                       className={`${classes.vendorStyle} ${classes.genStyle}`}
                     >
-                      {vendor}
+                      {story.type}
                     </Box>
                   </Box>
                   <Box component='p' fontSize='16px' fontWeight='400'>
-                    {message}
+                    {story.story}
                   </Box>
                 </Grid>
-              )
-            )}
-          </Grid>
+              ))}
+            </Grid>
+          </Box>
         </Box>
       </Box>
-    </Box>
+      <AddNewStory
+        openModal={openModal}
+        handleClose={handleClose}
+        storyList={storyList}
+        setStoryList={setStoryList}
+      />
+    </>
   )
 }
 
